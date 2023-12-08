19086244fca03a90a933d5a0a82ae9051f1aa906
S0507 <S0507@19S0507>
2023-12-08 10:23:44 +0900

    i

--- Imaquest/src/main/java/com/example/demo/controller/LoginController.java
+++ Imaquest/src/main/java/com/example/demo/controller/LoginController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
@@ -20,14 +22,21 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String login(@RequestParam String character_Name, @RequestParam String password, Model model) {
+    public String login(@RequestParam String character_Name, @RequestParam String password, Model model, HttpSession session) {
         // プレイヤー名とパスワードが一致するか確認
         String sqlCheckLogin = "SELECT COUNT(*) FROM player_characters WHERE character_Name = ? AND player_pass = ?";
         int count = jdbcTemplate.queryForObject(sqlCheckLogin, Integer.class, character_Name, password);
 
         if (count > 0) {
+        	
+        	
+            // ログイン成功時にセッションデータをデータベースに保存
+            String sessionId = session.getId();
+            String insertSql = "INSERT INTO session_data (session_id, character_name) VALUES (?, ?)";
+            jdbcTemplate.update(insertSql, sessionId, character_Name);
+
             // ログイン成功
-            return "field"; // ログイン後のリダイレクト先（例としてプレイヤーリスト画面）
+            return "field"; // ログイン後のリダイレクト先（例としてフィールド画面）
         } else {
             // ログイン失敗時のエラーメッセージを設定してログイン画面を再表示
             model.addAttribute("errorMessage", "プレイヤー名またはパスワードが正しくありません。");
