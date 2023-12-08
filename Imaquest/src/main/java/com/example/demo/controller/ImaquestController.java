fd117085dddabbe61431299edbdc52f28e34cb1b
S0506 <S0506@19S0506>
2023-12-08 10:17:50 +0900

    opening画面１

--- Imaquest/src/main/java/com/example/demo/controller/ImaquestController.java
+++ Imaquest/src/main/java/com/example/demo/controller/ImaquestController.java
@@ -1,51 +1,51 @@
-package com.example.demo.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-public class ImaquestController {
-
-    @GetMapping("/")
-    public String home() {
-        return "index"; // index.htmlのパス
-    }
-
-    @RequestMapping("/login")
-    public String loginForm(Model model) {
-        return "login"; // login.htmlのパス
-    }
-
-    @RequestMapping("/register")
-    public String registerForm(Model model) {
-        return "register"; // register.htmlのパス
-    }
-
-    // 新しく追加されたコントローラー
-    @RequestMapping("/demofield")
-    public String demoField(Model model) {
-        return "demofield"; // demofield.htmlのパス
-    }
-
-    @RequestMapping("/menu")
-    public String menu(Model model) {
-        return "menu"; // menu.htmlのパス
-    }
-
-    @RequestMapping("/mura")
-    public String mura(Model model) {
-        return "mura"; // mura.htmlのパス
-    }
-
-    @RequestMapping("/machi")
-    public String machi(Model model) {
-        return "machi"; // machi.htmlのパス
-    }
-
-    @RequestMapping("/battle")
-    public String battle(Model model) {
-        return "battle"; // battle.htmlのパス
-    }
-}
+//package com.example.demo.controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//@Controller
+//public class ImaquestController {
+//
+//    @GetMapping("/")
+//    public String home() {
+//        return "index"; // index.htmlのパス
+//    }
+//
+//    @RequestMapping("/login")
+//    public String loginForm(Model model) {
+//        return "login"; // login.htmlのパス
+//    }
+//
+//    @RequestMapping("/register")
+//    public String registerForm(Model model) {
+//        return "register"; // register.htmlのパス
+//    }
+//
+//    // 新しく追加されたコントローラー
+//    @RequestMapping("/demofield")
+//    public String demoField(Model model) {
+//        return "demofield"; // demofield.htmlのパス
+//    }
+//
+//    @RequestMapping("/menu")
+//    public String menu(Model model) {
+//        return "menu"; // menu.htmlのパス
+//    }
+//
+//    @RequestMapping("/mura")
+//    public String mura(Model model) {
+//        return "mura"; // mura.htmlのパス
+//    }
+//
+//    @RequestMapping("/machi")
+//    public String machi(Model model) {
+//        return "machi"; // machi.htmlのパス
+//    }
+//
+//    @RequestMapping("/battle")
+//    public String battle(Model model) {
+//        return "battle"; // battle.htmlのパス
+//    }
+//}
--- Imaquest/src/main/java/com/example/demo/controller/LoginController.java
+++ Imaquest/src/main/java/com/example/demo/controller/LoginController.java
@@ -22,12 +22,18 @@ public class LoginController {
     @PostMapping("/login")
     public String login(@RequestParam String character_Name, @RequestParam String password, Model model) {
         // プレイヤー名とパスワードが一致するか確認
-        String sqlCheckLogin = "SELECT COUNT(*) FROM player_characters WHERE character_Name = ? AND player_pass = ?";
+        String sqlCheckLogin = "SELECT COUNT(*) FROM player_characters WHERE character_Name = ? AND player_pass = ? ";
         int count = jdbcTemplate.queryForObject(sqlCheckLogin, Integer.class, character_Name, password);
 
         if (count > 0) {
             // ログイン成功
-            return "demofield"; // ログイン後のリダイレクト先（例としてプレイヤーリスト画面）
+
+            // ログイン後、first_loginの値を1以外に変更
+            String updateFirstLoginSql = "UPDATE player_characters SET first_login = 0 WHERE character_Name = ?";
+            jdbcTemplate.update(updateFirstLoginSql, character_Name);
+
+            // first_loginが1の場合、opening.htmlを表示
+            return (count == 1) ? "opening" : "field";
         } else {
             // ログイン失敗時のエラーメッセージを設定してログイン画面を再表示
             model.addAttribute("errorMessage", "プレイヤー名またはパスワードが正しくありません。");
