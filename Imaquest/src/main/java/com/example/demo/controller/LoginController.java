3228578a1b632b20a94ef3de79a992f64a377aeb
S0507 <S0507@19S0507>
2023-12-13 10:48:05 +0900

    1213

--- Imaquest/src/main/java/com/example/demo/controller/LoginController.java
+++ Imaquest/src/main/java/com/example/demo/controller/LoginController.java
@@ -1,4 +1,5 @@
 package com.example.demo.controller;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,14 +9,18 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
 @Controller
 public class LoginController {
+
     @Autowired
     private JdbcTemplate jdbcTemplate;
+
     @GetMapping("/login")
     public String showLoginForm() {
         return "login";
     }
+
     @PostMapping("/login")
     public String login(
             @RequestParam String character_Name,
@@ -29,9 +34,11 @@ public class LoginController {
             String sessionId = session.getId();
             String insertSql = "INSERT INTO session_data (session_id, character_name) VALUES (?, ?)";
             jdbcTemplate.update(insertSql, sessionId, character_Name);
+
             // Check the value of the first_login column
             String checkFirstLoginSql = "SELECT first_login FROM player_characters WHERE character_Name = ?";
             int firstLogin = jdbcTemplate.queryForObject(checkFirstLoginSql, Integer.class, character_Name);
+
             if (firstLogin == 1) {
                 // If first_login is 1, display opening.html
                 String updateFirstLoginSql = "UPDATE player_characters SET first_login = 5 WHERE character_Name = ?";
@@ -42,8 +49,7 @@ public class LoginController {
                 return "field";
             }
         } else {
-            model.addAttribute("errorMessage", "プレイヤー名またはパスワードが正しくありません。");
-            return "login";
+        	return "redirect:/ng";
         }
 		
     }
---
+++ Imaquest/src/main/resources/static/js/ng.js
@@ -0,0 +1,8 @@
+function returnToLogin() {
+        // ログイン画面に遷移する処理を追加
+        window.location.href = "/login";
+}
+function goToSignUp() {
+  
+    window.location.href = "register";
+}
